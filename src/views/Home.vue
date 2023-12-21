63a388656c09783dc1323660b0b2d242c0662e99
yuanbaoyaoyao <583515921@qq.com>
2023-12-21 22:58:03 +0800

    set basic logic of buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,92 +1,175 @@
 <script setup lang="ts">
-const data = [
+import Burger from '../assets/Burger.png'
+import Fries from '../assets/Fries.png'
+import Hotdog from '../assets/Hotdog.png'
+import Taco from '../assets/Taco.png'
+import Pizza from '../assets/Pizza.png'
+import Donut from '../assets/Donut.png'
+import Popcorn from '../assets/Popcorn.png'
+import Cake from '../assets/Cake.png'
+import Coke from '../assets/Coke.png'
+import Icecream from '../assets/Icecream.png'
+import Cookie from '../assets/Cookie.png'
+import Flan from '../assets/Flan.png'
+import { ref } from 'vue'
+let data = ref(
+  [
     {
-        name: "Burger",
-        money: "4.99",
-        src: "../assets/Burger.png"
+      name: "Burger",
+      money: "4.99",
+      src: Burger,
+      number: 0
     },
     {
-        name: "Fries",
-        money: "1.49",
-        src: "../assets/Fires.png"
+      name: "Fries",
+      money: "1.49",
+      src: Fries,
+      number: 0
     },
     {
-        name: "Hotdog",
-        money: "3.49",
-        src: "../assets/Hotdog.png"
+      name: "Hotdog",
+      money: "3.49",
+      src: Hotdog,
+      number: 0
     },
     {
-        name: "Taco",
-        money: "3.99",
-        src: "../assets/Taco.png"
+      name: "Taco",
+      money: "3.99",
+      src: Taco,
+      number: 0
     },
     {
-        name: "Pizza",
-        money: "7.99",
-        src: "../assets/Pizza.png"
+      name: "Pizza",
+      money: "7.99",
+      src: Pizza,
+      number: 0
     },
     {
-        name: "Donut",
-        money: "1.49",
-        src: "../assets/Donut.png"
+      name: "Donut",
+      money: "1.49",
+      src: Donut,
+      number: 0
     },
     {
-        name: "Popcorn",
-        money: "1.99",
-        src: "../assets/Popcorn.png"
+      name: "Popcorn",
+      money: "1.99",
+      src: Popcorn,
+      number: 0
     },
     {
-        name: "Coke",
-        money: "1.49",
-        src: "../assets/Coke.png"
+      name: "Coke",
+      money: "1.49",
+      src: Coke,
+      number: 0
     },
     {
-        name: "cake",
-        money: "10.99",
-        src: "../assets/Burger.png"
+      name: "Cake",
+      money: "10.99",
+      src: Cake,
+      number: 0
     },
     {
-        name: "Icecream",
-        money: "5.99",
-        src: "../assets/Burger.png"
+      name: "Icecream",
+      money: "5.99",
+      src: Icecream,
+      number: 0
     },
     {
-        name: "Cookie",
-        money: "3.99",
-        src: "../assets/Burger.png"
+      name: "Cookie",
+      money: "3.99",
+      src: Cookie,
+      number: 0
     },
     {
-        name: "Flan",
-        money: "7.99",
-        src: "../assets/Burger.png"
+      name: "Flan",
+      money: "7.99",
+      src: Flan,
+      number: 0
     },
-]
+  ]
+)
+
+const handleClickAddButton = (index: number) => {
+  data.value[index].number++
+}
+const handleClickMinusButton = (index: number) => {
+  data.value[index].number--
+}
+
 </script>
 
 <template>
-    <div class="content">
-        <div class="content-details" v-for="(item, index) in data">
-            <img src="../assets/Burger.png" width="90" height="90" />
-            <text>
-                {{ item.name }}-{{ item.money }}
-            </text>
+  <div class="content">
+    <div class="content-details" v-for="(item, index) in data">
+      <img :src=item.src width="90" height="90" />
+      <text class="content-details-text">
+        <text>{{ item.name }} </text>
+        ·
+        <text class="content-details-text-money">${{ item.money }} </text>
+      </text>
+      <div style="width: 100%;display: flex;justify-content: center;">
+        <el-button v-if="item.number == 0" class="content-button" type="warning"
+          @click="handleClickAddButton(index)">ADD</el-button>
+        <div v-else class="content-multi-buttons">
+          <el-button style="font-size: 30px;" class="content-button" type="danger"
+            @click="handleClickMinusButton(index)">-</el-button>
+          <el-button style="font-size: 30px;" class="content-button" type="warning"
+            @click="handleClickAddButton(index)">+</el-button>
         </div>
+      </div>
     </div>
+  </div>
+  <div class="bottom">
+    <text>@DurgerKingBot</text>
+  </div>
 </template>
 
 <style scoped>
 .content {
-    color: black;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    background-color: blue;
+  color: black;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 }
 
 .content-details {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+.content-details-text {
+  margin-top: 3px;
+  margin-bottom: 8px;
+}
+
+.content-details-text-money {
+  font-weight: 600;
+}
+
+.content-multi-buttons {
+  display: flex;
+  width: 80%;
+}
+
+.content-button {
+  width: 60%;
+  font-weight: 700;
+  border-radius: 8px;
+}
+
+.bottom {
+  color: #b8b8b8;
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 10000;
+  bottom: 0;
 }
 </style>
